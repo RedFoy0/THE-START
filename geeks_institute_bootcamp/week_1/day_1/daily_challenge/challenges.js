aaab22ae67e3fd3ce60b94ee069eae971f32c687
redahhjokin <redamohamedelkhchin56@gmail.com>
2026-01-29 15:59:41 +0100

    modified:   geeks_institute_bootcamp/week_1/day_1/daily_challenge/challenges.js

--- geeks_institute_bootcamp/week_1/day_1/daily_challenge/challenges.js
+++ geeks_institute_bootcamp/week_1/day_1/daily_challenge/challenges.js
@@ -1,5 +1,33 @@
-// ====== Daily Challenge 1
+// ====== Daily Challenge 1 ======
 
-// ====== Daily Challenge 2
+// ----------- Not Bad -------------
 
-// ====== Daily Challenge 3
+let sentence = prompt("that contains the words “not” and “bad”");
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
+
+if (wordNot !== -1 && wordBad !== -1 && wordNot < wordBad) {
+  console.log("kayna");
+  let final = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+  console.log(final);
+} else {
+  console.log(sentence);
+}
+
+// ====== Daily Challenge 2 ======
+
+// ----------- Stars -------------
+
+let ktaba = "";
+for (let i = 0; i < 6; i++) {
+  ktaba = ktaba + "* ";
+  console.log(ktaba);
+}
+
+ktaba = "";
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j <= i; j++) {
+    ktaba = ktaba + "* ";
+    console.log(ktaba);
+  }
+}
