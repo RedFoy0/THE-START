63f42b581c78ca33bb5651f660ca6053a356ca09
redahhjokin <redamohamedelkhchin56@gmail.com>
2026-02-04 10:24:48 +0100

    modified:   geeks_institute_bootcamp/week_1/day_2/daily_challenge/challenges.js

--- geeks_institute_bootcamp/week_1/day_2/daily_challenge/challenges.js
+++ geeks_institute_bootcamp/week_1/day_2/daily_challenge/challenges.js
@@ -1,5 +1,45 @@
-// ====== Daily Challenge 1
+// ====== Daily Challenge 1 ======
 
-// ====== Daily Challenge 2
+// ----------- Planets -------------
+let planets = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune",
+];
 
-// ====== Daily Challenge 3
+let colors = [
+  "brown",
+  "gray",
+  "blue",
+  "red",
+  "orange",
+  "gold",
+  "lightblue",
+  "darkblue",
+];
+
+let moons = [0, 0, 1, 2, 4, 8, 7, 4];
+
+const section = document.querySelector(".listPlanets");
+
+for (let i = 0; i < planets.length; i++) {
+  const planetDiv = document.createElement("div");
+  planetDiv.setAttribute("class", "planet");
+  //   planetDiv.textContent = planets[i];
+  planetDiv.style.backgroundColor = colors[i];
+  section.appendChild(planetDiv);
+
+  for (let j = 0; j < moons[i]; j++) {
+    const moonDiv = document.createElement("div");
+    moonDiv.setAttribute("class", "moon");
+    moonDiv.style.backgroundColor = "white";
+    moonDiv.style.top = 45 + "px";
+    moonDiv.style.left = j * 30 + "px";
+    planetDiv.appendChild(moonDiv);
+  }
+}
