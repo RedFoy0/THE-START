93daf9c8982356971485c87cf5bdd1d57e635e70
redahhjokin <redamohamedelkhchin56@gmail.com>
2026-02-05 22:33:12 +0100

    Implement interactive features in exercises_xp.js
    Added event listeners for changing colors, hiding elements, and making text bold. Included functionality for mouseover to change font size.

--- geeks_institute_bootcamp/week_1/day_3/exercises/exercises_xp.js
+++ geeks_institute_bootcamp/week_1/day_3/exercises/exercises_xp.js
@@ -1,6 +1,54 @@
 // ===== Exercise 1 =====
 
 // ----------- Change the article -------------
+let firstH = document.getElementsByTagName("h1")[0];
+console.log(firstH);
+
+let Article = document.getElementsByTagName("article")[0];
+let lastpara = Article.lastElementChild
+Article.removeChild(lastpara)
+
+let secondH = document.getElementsByTagName("h2")[0];
+secondH.addEventListener("click" , changeColor)
+function changeColor() {
+    secondH.style.backgroundColor = "red"
+}
+
+let thirdH = document.getElementsByTagName("h3")[0];
+thirdH.addEventListener("click" , hide)
+function hide() {
+    thirdH.style.display = "none"
+
+}
+
+let btn = document.createElement("button")
+btn.textContent = "bold"
+let body = document.getElementsByTagName("body")[0]
+body.appendChild(btn)
+
+document.addEventListener("click" , bold)
+function bold() {
+    let paragraphs = document.querySelectorAll("p");
+
+  for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.fontWeight = "bold";
+  }
+}
+
+
+// bonus 1
+
+let randomNum = Math.floor(Math.random() * 100);
+
+firstH.addEventListener("mouseover" , changeFontSize)
+function changeFontSize() {
+    firstH.style.fontSize = randomNum + "px"
+}
+
+console.log (randomNum)
+
+// bonus 2
+
 
 // ===== Exercise 2 =====
 
