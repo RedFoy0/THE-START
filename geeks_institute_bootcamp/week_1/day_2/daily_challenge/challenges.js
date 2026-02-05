b5c1428216be5044654123afb06062cb3c4a64f1
redahhjokin <redamohamedelkhchin56@gmail.com>
2026-02-05 12:28:55 +0100

    modified:   geeks_institute_bootcamp/week_1/day_2/daily_challenge/challenges.js

--- geeks_institute_bootcamp/week_1/day_2/daily_challenge/challenges.js
+++ geeks_institute_bootcamp/week_1/day_2/daily_challenge/challenges.js
@@ -47,3 +47,32 @@ for (let i = 0; i < planets.length; i++) {
 // ====== Daily Challenge 2 ======
 
 // ----------- Words in the stars -------------
+// Words in the stars
+
+let SevWords = prompt("write several words (separated by commas)");
+
+let words = SevWords.split(", ");
+
+let maxLen = 0;
+for (let i = 0; i < words.length; i++) {
+  if (words[i].length > maxLen) {
+    maxLen = words[i].length;
+  }
+}
+
+let stars = "";
+for (let i = 0; i < maxLen + 4; i++) {
+  stars = stars + "*";
+}
+console.log(stars);
+
+for (let i = 0; i < words.length; i++) {
+  let space = "";
+  let spacelen = maxLen - words[i].length;
+  for (let j = 0; j < spacelen; j++) {
+    space = space + " ";
+  }
+  console.log("* " + words[i] + space + " *");
+}
+
+console.log(stars);
