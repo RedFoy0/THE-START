ba329a0b80393441c5a514a1f7a5d210c87fefc8
redahhjokin <redamohamedelkhchin56@gmail.com>
2026-02-06 22:02:36 +0100

    modified:   geeks_institute_bootcamp/week_1/day_3/daily_challenges/challenges.js

--- geeks_institute_bootcamp/week_1/day_3/daily_challenges/challenges.js
+++ geeks_institute_bootcamp/week_1/day_3/daily_challenges/challenges.js
@@ -2,6 +2,56 @@
 
 // ----------- Tell the story -------------
 
+let form = document.getElementById("libform");
+let story = document.getElementById("story");
+let noun = document.getElementById("noun");
+let adjective = document.getElementById("adjective");
+let person = document.getElementById("person");
+let verb = document.getElementById("verb");
+let place = document.getElementById("place");
+let btn = document.getElementById("lib-button");
+
+btn.textContent = "darija";
+btn.addEventListener("click", goll9isa);
+let engBtn = document.createElement("button");
+engBtn.textContent = "english";
+form.appendChild(engBtn);
+engBtn.addEventListener("click", tellTheStory);
+
+function tellTheStory(e) {
+  e.preventDefault();
+  let al9isa =
+    " hi my name is " +
+    noun.value +
+    " i am a " +
+    adjective.value +
+    " today i found a new friend named" +
+    person.value +
+    " that person  " +
+    verb.value +
+    " in " +
+    place.value;
+  console.log(al9isa);
+  story.textContent = al9isa;
+}
+
+function goll9isa(e) {
+  e.preventDefault();
+  let al9isa =
+    " kan ya makan fi 9adimi zaman kana honaka cha5son yod3a " +
+    noun.value +
+    " kana yo3rafo bi anho " +
+    adjective.value +
+    " wa fi yawmin mina l 2ayam ilta9a bi " +
+    person.value +
+    " wajada dalika cha5s " +
+    verb.value +
+    " fi " +
+    place.value;
+  console.log(al9isa);
+  story.textContent = al9isa;
+}
+
 // ====== Daily Challenge 2 ======
 
 // ----------- Letters -------------
