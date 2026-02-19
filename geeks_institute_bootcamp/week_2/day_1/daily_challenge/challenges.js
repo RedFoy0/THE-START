82e3e915ee73420954153cdde89754fc8b8ea37a
RedFoy0 <redamohamedelkhchin56@gmail.com>
2026-02-19 07:04:17 +0000

    modified:   geeks_institute_bootcamp/week_2/day_1/daily_challenge/challenges.js

--- geeks_institute_bootcamp/week_2/day_1/daily_challenge/challenges.js
+++ geeks_institute_bootcamp/week_2/day_1/daily_challenge/challenges.js
@@ -2,4 +2,47 @@
 
 // ----------- Groceries-------------
 
+let client = "John";
+
+const groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "20$",
+    other : {
+        paid : true,
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
+
+
+const displayGroceries = () => {
+    groceries.fruits.forEach(fruit => {
+        console.log(fruit);
+    });
+};
+displayGroceries();
+
+const cloneGroceries = () => {
+
+   
+    let user = client;
+
+
+    client = "Betty";
+
+    console.log("client:", client); 
+    console.log("user:", user);     
+
+
+    let shopping = groceries;
+
+    groceries.totalPrice = "35$";
+
+    console.log("shopping total:", shopping.totalPrice); 
+
+
+    groceries.other.paid = false;
+
+    console.log("shopping paid:", shopping.other.paid); 
+};
 
