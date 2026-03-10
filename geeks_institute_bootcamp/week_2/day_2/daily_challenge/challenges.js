bbb424dd1eb04b95c2c8db2ec47c3d83f4e71b06
RedFoy0 <redamohamedelkhchin56@gmail.com>
2026-03-10 19:40:39 +0000

    deleted:    geeks_institute_bootcamp/week_2/day_2/daily_challenge 	new file:   geeks_institute_bootcamp/week_2/day_2/daily_challenge/challenges.js 	new file:   geeks_institute_bootcamp/week_2/day_2/exercises/exercises_xp_ninja.js

---
+++ geeks_institute_bootcamp/week_2/day_2/daily_challenge/challenges.js
@@ -0,0 +1,79 @@
+// ====== Daily Challenge 1 ======
+
+// ----------- Go Wildcats-------------
+
+const gameInfo = [
+    {
+        username: "john",
+        team: "red",
+        score: 5,
+        items: ["ball", "book", "pen"]
+    },
+    {
+        username: "becky",
+        team: "blue",
+        score: 10,
+        items: ["tape", "backpack", "pen"]
+    },
+    {
+        username: "susy",
+        team: "red",
+        score: 55,
+        items: ["ball", "eraser", "pen"]
+    },
+    {
+        username: "tyson",
+        team: "green",
+        score: 1,
+        items: ["book", "pen"]
+    },
+];
+
+const usernames = [];
+gameInfo.forEach(user => {
+    usernames.push(user.username + "!");
+});
+
+console.log("Usernames:", usernames);
+
+
+const winners = [];
+gameInfo.forEach(user => {
+    if (user.score > 5) {
+        winners.push(user.username);
+    }
+});
+
+console.log("Winners:", winners);
+
+
+let totalScore = 0;
+gameInfo.forEach(user => {
+    totalScore += user.score;
+});
+
+console.log("Total Score:", totalScore);
+
+// ====== Daily Challenge 2 ======
+
+// ----------- Car Inventory -------------
+
+const inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
+
+function getCarHonda(carInventory) {
+  const car = carInventory.find(c => c.car_make === "Honda");
+  return `This is a ${car.car_make} ${car.car_model} from ${car.car_year}.`;
+}
+
+function sortCarInventoryByYear(carInventory) {
+  return [...carInventory].sort((a, b) => a.car_year - b.car_year);
+}
+
+console.log(getCarHonda(inventory));
+console.log(sortCarInventoryByYear(inventory));
