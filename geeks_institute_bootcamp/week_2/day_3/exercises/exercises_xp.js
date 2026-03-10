3d8f047184be64f14913ccc746185c0efde0cb0d
RedFoy0 <redamohamedelkhchin56@gmail.com>
2026-03-10 19:45:04 +0000

    new file:   geeks_institute_bootcamp/week_2/day_3/daily_challenge/challenges.js 	new file:   geeks_institute_bootcamp/week_2/day_3/exercises/exercises_xp.js 	new file:   geeks_institute_bootcamp/week_2/day_3/exercises/exercises_xp_gold.js 	new file:   geeks_institute_bootcamp/week_2/day_3/exercises/exercises_xp_ninja.js

---
+++ geeks_institute_bootcamp/week_2/day_3/exercises/exercises_xp.js
@@ -0,0 +1,121 @@
+// // ===== Exercise 1 =====
+
+// // ------------- Colors -------------
+
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+
+colors.forEach((color, index) => {
+    console.log(`${index + 1}# choice is ${color}.`);
+});
+
+if (colors.includes("Violet")) {
+    console.log("Yeah");
+} else {
+    console.log("No...");
+}
+
+// ===== Exercise 2 =====
+
+// ------------- Colors #2 -------------
+
+const colorss = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+const ordinal = ["th", "st", "nd", "rd"];
+
+colorss.forEach((color, index) => {
+    let position = index + 1;
+
+    let suffix =
+    position === 1 ? ordinal[1] :
+    position === 2 ? ordinal[2] :
+    position === 3 ? ordinal[3] :
+    ordinal[0];
+
+    console.log(`${position}${suffix} choice is ${color}.`);
+});
+
+// ===== Exercise 3 =====
+
+// ----------- Analyzing -------------
+
+const fruits = ["apple", "orange"];
+const vegetables = ["carrot", "potato"];
+
+const result = ['bread', ...vegetables, 'chicken', ...fruits];
+console.log(result);
+// output:
+// ["bread", "carrot", "potato", "chicken", "apple", "orange"]
+
+const country = "USA";
+console.log([...country]);
+// output:
+// ["U", "S", "A"]
+
+
+let newArray = [...[,,]];
+console.log(newArray);
+//output:
+// [undefined, undefined]
+
+// ===== Exercise 4 =====
+
+// ----------- Employees -------------
+
+const users = [
+  { firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
+  { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
+  { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
+  { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
+  { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
+  { firstName: 'Wes', lastName: 'Reid', role: 'Instructor' },
+  { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor' }
+];
+
+
+const welcomeStudents = users.map(user => `Hello ${user.firstName}`);
+console.log(welcomeStudents);
+
+
+const fullStackResidents = users.filter(
+  user => user.role === 'Full Stack Resident'
+);
+console.log(fullStackResidents);
+
+
+const residentLastNames = users
+  .filter(user => user.role === 'Full Stack Resident')
+  .map(user => user.lastName);
+
+console.log(residentLastNames);
+
+
+// ===== Exercise 5 =====
+
+// ----------- Star Wars -------------
+
+const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
+
+const sentence = epic.reduce((acc, word) => acc + ' ' + word);
+console.log(sentence);
+
+// ===== Exercise 6 =====
+
+// ----------- Employees #2 -------------
+
+const students = [
+  {name: "Ray", course: "Computer Science", isPassed: true}, 
+  {name: "Liam", course: "Computer Science", isPassed: false}, 
+  {name: "Jenner", course: "Information Technology", isPassed: true}, 
+  {name: "Marco", course: "Robotics", isPassed: true}, 
+  {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
+  {name: "Jamie", course: "Big Data", isPassed: false}
+];
+
+
+const passedStudents = students.filter(student => student.isPassed);
+console.log(passedStudents);
+
+students
+  .filter(student => student.isPassed)
+  .forEach(student => {
+    console.log(`Good job ${student.name}, you passed the course in ${student.course}`);
+  });
